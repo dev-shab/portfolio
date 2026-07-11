8936f659555adbcb1b2b9443b09d2bbfc086ad70
dev-shab <shabreeshnair1998@gmail.com>
2026-07-11 16:33:44 +0530

    Configure GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ if ('scrollRestoration' in window.history) {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
